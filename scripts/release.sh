e5b86d255ce7199720f61998a7f80becc682ac2f
Gavyn Riebau <gavyn.riebau@gmail.com>
2023-11-08 10:48:42 +0800

    Initial release v1.0.0+1
    Basic functionality of viewing list/details for articles
    is working but some functionality such as viewing article
    author is missing.

---
+++ scripts/release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Grab the version number from the pubspec file.
+VERSION=$(cat pubspec.yaml | grep '^version' | cut -d' ' -f2 | sed 's/\+/_/g')
+
+flutter build apk --release
+cp build/app/outputs/flutter-apk/app-release.apk simple-bbc-news-reader-v$VERSION.apk
+gh release create "$VERSION" simple-bbc-news-reader-v$VERSION.apk
